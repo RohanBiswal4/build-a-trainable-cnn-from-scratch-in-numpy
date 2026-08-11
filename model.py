bd4e6c1888781ce1fd1a20d8c62edd98c4f10f7d
Rohan_Biswal <rmanit1996@gmail.com>
2026-08-11 13:37:40 +0530

    Step 12: init_zero_bias

--- model.py
+++ model.py
@@ -80,8 +80,12 @@ def he_init(shape, fan_in, seed):
     sigma=he_std(fan_in)
     return np.random.normal(0,sigma,shape)
 
-# Step 12 - init_zero_bias (not yet solved)
-# TODO: implement
+# Step 12 - init_zero_bias
+import numpy as np
+
+def init_zero_bias(length):
+    # TODO: return a 1D float array of zeros with the given length.
+    return np.zeros(length,dtype='float64')
 
 # Step 13 - pad_2d (not yet solved)
 # TODO: implement

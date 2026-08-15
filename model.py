c0c8313b69731173f8eb0242872c0c79b18ef81b
Rohan_Biswal <rmanit1996@gmail.com>
2026-08-15 16:52:26 +0530

    Step 38: adam_update_v

--- model.py
+++ model.py
@@ -374,8 +374,12 @@ def adam_update_m(m, grad, beta_one):
     # TODO: return the updated first moment estimate using beta_one and grad.
     return beta_one*m +(1-beta_one)*grad
 
-# Step 38 - adam_update_v (not yet solved)
-# TODO: implement
+# Step 38 - adam_update_v
+import numpy as np
+
+def adam_update_v(v, grad, beta_two):
+    # TODO: return the updated Adam second moment estimate as an EMA of squared gradients.
+    return beta_two*v +(1-beta_two)*(grad**2)
 
 # Step 39 - adam_bias_correct (not yet solved)
 # TODO: implement
